1db6c692aa3eff00a733fc339fa92426bc4b626a
Zixuan Guo <i@zxguo.me>
2021-05-31 01:23:56 +0800

    add modal

--- frontend/src/CardList.js
+++ frontend/src/CardList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 import './App.css';
-// import { List } from 'antd';
+import {Statistic, Row, Col, Modal} from 'antd';
 import Broadcast from "./Broadcast";
 import VirtualScroller from "virtual-scroller/react";
 
@@ -11,6 +11,8 @@ export default class CardList extends React.Component {
         loading: true,
         count: 0,
         data: [],
+        isModalVisible: false,
+        modalData: {},
     };
 
     componentDidMount() {
@@ -45,16 +47,59 @@ export default class CardList extends React.Component {
 
     render() {
         return (
-            <VirtualScroller
-                id="broadcasts"
-                items={this.state.data}
-                itemComponent={({children}) => (
-                    <div style={{padding: '14px 14px 8px'}}>
-                        <Broadcast item={children}></Broadcast>
-                    </div>
-                )}
-                getColumnsCount={(container) => (Math.min(Math.floor(container.getWidth() / 240), 4))}
-            />
+            <>
+                <VirtualScroller
+                    id="broadcasts"
+                    items={this.state.data}
+                    itemComponent={({children}) => (
+                        <div
+                            style={{padding: '14px 14px 8px'}}
+                            onClick={()=>(this.setState({
+                                isModalVisible: true,
+                                modalData: children,
+                            }))}
+                        >
+                            <Broadcast item={children}>
+                            </Broadcast>
+                        </div>
+                    )}
+                    getColumnsCount={(container) => (Math.min(Math.floor(container.getWidth() / 240), 4))}
+                />
+                <Modal
+                    visible={this.state.isModalVisible}
+                    centered={true}
+                    onCancel={() => {
+                        this.setState({isModalVisible: false})
+                    }}
+                    footer={null}
+                >
+                    <Row gutter={16}>
+                        <Col>
+                            <Statistic title="开始时间" value={(() => {
+                                let time = new Date(this.state.modalData.livetime)
+                                return time.toLocaleString('zh-CN')
+                            })()
+                            }/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={10}>
+                            <Statistic title="当前人气" value={this.state.modalData.popularity}/>
+                        </Col>
+                        <Col span={5}>
+                            <Statistic title="最高人气" value={this.state.modalData.maxPopularity}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={10}>
+                            <Statistic title="金瓜子" value={this.state.modalData.goldCoin}/>
+                        </Col>
+                        <Col span={5}>
+                            <Statistic title="银色瓜子" value={this.state.modalData.silverCoin}/>
+                        </Col>
+                    </Row>
+                </Modal>
+            </>
         );
     }
 }
